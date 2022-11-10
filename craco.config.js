b3292504615fda5ccb35f344ac1ac03c31c2729e
김영균 <kyoungyun98@naver.com>
2022-11-10 17:26:45 +0900

    initial commit

---
+++ craco.config.js
@@ -0,0 +1,23 @@
+const CracoAlias = require("craco-alias");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "options",
+        baseUrl: "./",
+        aliases: {
+          "@api": "./src/api",
+          "@assets": "./src/assets",
+          "@components": "./src/components",
+          "@hooks": "./src/hooks",
+          "@pages": "./src/pages",
+          "@style": "./src/style",
+          "@utils": "./src/utils",
+          "@routes": "./src/routes",
+        },
+      },
+    },
+  ],
+};
